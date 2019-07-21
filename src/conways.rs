406522a66db817f95b02cc1942aa9f3b9f72f4ca
Jacob Adams <jacob@tookmund.com>
2019-07-21 22:24:22 +0100

    conways: Use RC type for row and column type consistency also neighbors function

--- src/conways.rs
+++ src/conways.rs
@@ -1,18 +1,21 @@
 extern crate multiarray;
 
 use multiarray::Array2D;
+use std::usize::MAX;
+use std::num::Wrapping;
 
 type Board = Array2D<u8>;
+type RC = usize;
 
 pub struct Life {
-    rows: usize,
-    cols: usize,
+    rows: RC,
+    cols: RC,
     board: Board,
     wraparound: bool,
 }
 
 impl Life {
-    pub fn new(rows: usize, cols: usize) -> Self {
+    pub fn new(rows: RC, cols: RC) -> Self {
         Self {
             rows: rows,
             cols: cols,
@@ -20,5 +23,44 @@ impl Life {
             wraparound: true,
         }
     }
-
+    fn neighbors(&self, row: RC, column: RC) -> RC {
+        let mut total: RC = 0;
+        for lr in row.wrapping_sub(1)..row.wrapping_add(1) {
+            let mut r = lr;
+            if r == MAX {
+                if self.wraparound {
+                    r = self.rows-1;
+                }
+                else {
+                    continue;
+                }
+            }
+            if r > self.rows-1 {
+                if self.wraparound {
+                    r = 0;
+                }
+                else {
+                    continue;
+                }
+            }
+            for lc in column.wrapping_sub(1)..column.wrapping_add(1) {
+                let mut c = lc;
+                if r == row && c == column {
+                    continue;
+                }
+                if c == MAX {
+                    if self.wraparound {
+                        c = self.cols-1;
+                    }
+                    else {
+                        continue;
+                    }
+                }
+                if self.board[[r,c]] > 0 {
+                    total += 1;
+                }
+            }
+        }
+    total
+    }
 }
