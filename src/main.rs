20dd30c80d7bf7f623a129cdcae69b6911d25cc4
Jacob Adams <jacob@tookmund.com>
2019-07-21 23:49:04 +0100

    main: Print the game to screen

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
+use std::io::{self, Write};
 mod conways;
 
 fn main() {
     println!("Begin");
-    let con = conways::Life::new(50, 50);
+    let mut con = conways::Life::new(10, 10);
+    con.populate();
+    loop {
+        for r in 0..con.rows {
+            for c in 0..con.cols {
+                if con.is_alive(r, c) {
+                    print!("+");
+                }
+                else {
+                    print!("-");
+                }
+            }
+            println!("");
+            io::stdout().flush().unwrap();
+        }
+        con.next();
+
+    }
 }
